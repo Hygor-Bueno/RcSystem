a153ce92e66c7c7a824a6265d337d85fd4b9778a
Hygor-Bueno <hygor_knd.x1@hotmail.com>
2024-02-14 16:57:35 -0300

    up

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -2,7 +2,7 @@ import Loading from "../Components/Loading/Loading";
 import { useMyContext } from "../MyContext";
 import FormProduct from "../Components/Form/FormProduct";
 import Modal from "../Components/Modal/Modal";
-import { faBox,faSackDollar,faListCheck } from '@fortawesome/free-solid-svg-icons'
+import { faBox,faChartPie,faListCheck } from '@fortawesome/free-solid-svg-icons'
 import Buttons from "../Components/Buttons/Buttons";
 
 export default function Dashboard(): JSX.Element {
@@ -15,8 +15,8 @@ export default function Dashboard(): JSX.Element {
                 <FormProduct />
             </Modal>
             <Buttons typeBtn="button" onAction={()=>setModal(true)} title="Cadastrar Produtos" iconBtn={faBox} classBtn="m-2 btn btn-success"/>
-            <Buttons typeBtn="button" onAction={()=>console.log('Cadastrar Classe de Produtos')} title="Cadastrar Produtos" iconBtn={faListCheck} classBtn="m-2 btn btn-primary"/>
-            <Buttons typeBtn="button" onAction={()=>console.log('Cadastrar Mesas')} title="Cadastrar Produtos" iconBtn={faSackDollar} classBtn="m-2 btn btn-primary"/>
+            <Buttons typeBtn="button" onAction={()=>console.log('Cadastrar Classe de Produtos')} title="Cadastrar Produtos" iconBtn={faChartPie} classBtn="m-2 btn btn-primary"/>
+            <Buttons typeBtn="button" onAction={()=>console.log('Cadastrar Mesas')} title="Cadastrar Produtos" iconBtn={faListCheck} classBtn="m-2 btn btn-primary"/>
         </div>
     );
 }
